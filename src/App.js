1c7f54506590be9dfe61982af2bed680ddaaeadf
Cherisia <yhcyoon@kakao.com>
2024-05-21 17:47:07 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-   let data = '하이';
+    let [글제목, b] = useState(['안녕하세요', '반가워요', '하이요']);
     return (
         <div className="App">
             <div className="black-nav">
-                <h4>블로그</h4>
-                <p style={{color:'red',fontSize:'20px'}}>{data}</p>
+                <div>개발 blog</div>
+            </div>
+            <div className="list">
+                <h4>{ 글제목[0] }</h4>
+                <p>2월 17일 발행</p>
+            </div>
+            <div className="list">
+                <h4>{ 글제목[1] }</h4>
+                <p>2월 17일 발행</p>
+            </div>
+            <div className="list">
+                <h4>{ 글제목[2] }</h4>
+                <p>2월 17일 발행</p>
             </div>
         </div>
-    );
+    )
 }
 
 export default App;
