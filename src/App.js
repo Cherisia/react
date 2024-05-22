163fe404f52d78d75d46e93a316baa15116e0717
Cherisia <yhcyoon@kakao.com>
2024-05-22 17:49:53 +0900

    state 변경함수쓰면 자동으로 렌더링

--- src/App.js
+++ src/App.js
@@ -2,26 +2,34 @@ import './App.css';
 import {useState} from "react";
 
 function App() {
-    let [글제목, b] = useState(['안녕하세요', '반가워요', '하이요']);
+    let [title, setTitle] = useState(['안녕하세요', '반가워요', '하이요']);
+    let [like, setLike] = useState(0);
+
     return (
         <div className="App">
             <div className="black-nav">
                 <div>개발 blog</div>
             </div>
             <div className="list">
-                <h4>{ 글제목[0] }</h4>
+                <h4>{title[0]} <span onClick={() => {
+                    setLike(like + 1)
+                }}>👍</span> {like}</h4>
+                <button onClick={() => {
+                    setTitle(['이렇게바뀜', '반가워요', '하이요'])
+                }}>누르면 제목이 바껴요
+                </button>
                 <p>2월 17일 발행</p>
             </div>
             <div className="list">
-                <h4>{ 글제목[1] }</h4>
+                <h4>{title[1]}</h4>
                 <p>2월 17일 발행</p>
             </div>
             <div className="list">
-                <h4>{ 글제목[2] }</h4>
+                <h4>{title[2]}</h4>
                 <p>2월 17일 발행</p>
             </div>
         </div>
-    )
+    );
 }
 
 export default App;
